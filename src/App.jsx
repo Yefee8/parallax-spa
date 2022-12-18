b0dc3c5da95e0be9a1e09a94b828bd3679574aa1
yefee8 <yavuzefefindikli8@gmail.com>
2022-12-18 15:23:33 +0300

    first version 🥳

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css'
+import { Parallax } from 'react-scroll-parallax';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { useEffect } from 'react';
+function App() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <Parallax translateY={[-20, 20]} style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', paddingBottom: '15rem' }}>
+      <div className="bg">
+        <Parallax translateY={['-1200px', '1200px']}>
+          <h1 className="title">
+            for a relaxing
+            <br />
+            <span>journey,</span>
+            <br />
+            call us.
+          </h1>
+
+          <Parallax opacity={[-1, 3]}>
+            <h2 className="links">
+              <span>book a trip</span>
+
+              <div>
+                <FontAwesomeIcon onClick={()=>{
+                  window.open('https://github.com/Yefee8')
+                }} icon={faGithub} style={{ marginRight: '10px', color: '#FF8008' }} />
+                <FontAwesomeIcon onClick={()=>{
+                  window.open('https://instagram.com/yefee8')
+                }} icon={faInstagram} />
+              </div>
+            </h2>
+          </Parallax>
+        </Parallax>
+      </div>
+    </Parallax>
+  )
+}
+
+export default App
